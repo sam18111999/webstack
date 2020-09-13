c45d46ef434c260d4c6bdd75fd27a41eb154bebe
sam18111999 <68906254+sam18111999@users.noreply.github.com>
2020-09-13 19:33:39 +0530

    fourth week assignment updated

--- FWA/SpeakGoodBye.js
+++ FWA/SpeakGoodBye.js
@@ -1,4 +1,4 @@
-/ NOTE! The steps in this file are basically identical to the ones you
+// NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
@@ -11,26 +11,15 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-// var speakWord = "Good Bye";
+var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-// function speak(name) {
-//   console.log(speakWord + " " + name);
-// }
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
-
-(function (window) {
-  var byeSpeaker = {};
-  byeSpeaker.speakWord = "Good Bye";
-  byeSpeaker.speak = function (name) {
-    console.log(this.speakWord + " " + name);
-  }
-
-  window.byeSpeaker = byeSpeaker;
-
-})(window);
